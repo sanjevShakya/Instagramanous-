15c005eb8cd0546f2b45c686f3c60bf764f338d3
Sanjeev Shakya <sshakya@localhost.sanjev>
2017-01-24 18:45:33 +0545

    image adjustment functionalities

--- js/manipulators/Manipulator.js
+++ js/manipulators/Manipulator.js
@@ -37,14 +37,21 @@ var Manipulator = (function() {
 
 		}
 
+		this.setValue = function(_value) {
+			this.slider.setAttribute('value',_value);
+		}
+
 		this.setMinValue = function(_minValue) {
 			this.min = _minValue;
 			this.slider.setAttribute('min',_minValue);
 		}
 
 		this.setLabel = function(_label) {
-			console.log("label set to ",_label);
 			this.labelElem.innerHTML = _label;
+		}
+
+		this.setStep = function(_step) {
+			this.slider.setAttribute('step',_step);
 		}
 
 		this.append = function() {
@@ -121,7 +128,6 @@ var Brightness = (function() {
 	return {
 
 		getInstance: function() {
-			console.log('insde brightness');
 			if(instance == null) {
 				instance = new Brightness();
 			}
@@ -172,14 +178,20 @@ var Contrast = (function() {
 				}	
 				previousValue = currentValue;
 				// brightness change algorithm
+				var factor = getCorrectionFactor(step);
 				for(var i=0; i< data.length; i+=4) {
-					data[i] = Util.truncate(data[i] + step)//R
-					data[i+1] =Util.truncate(data[i+1] + step) //G
-					data[i+2] =Util.truncate(data[i+2] + step)//B
+					
+					data[i] = Util.truncate(factor * (data[i] - 128) + 128)//R
+					data[i+1] =Util.truncate(factor *(data[i+1] - 128) + 128) //G
+					data[i+2] =Util.truncate(factor *(data[i+2] -128) + 128)//B
 				}
 				context.putImageData(imageData, 0, 0);
 			}
 		}
+
+		var getCorrectionFactor = function(value) {
+			return ((259 * (value + 255)) / (255 * (259 - value)))
+		} 
 
 	}
 	return {
@@ -187,6 +199,373 @@ var Contrast = (function() {
 		getInstance: function() {
 			if(instance == null) {
 				instance = new Contrast();
+			}
+			return instance;
+		}
+	}
+})();
+
+var Gamma = (function() {
+	var instance;
+	function Gamma() {
+		var gamma = Manipulator.getInstance();
+		
+		this.init = function() {
+			gamma.init();
+			gamma.setId('gammaSlider');
+			gamma.setType('range');
+			gamma.setMaxValue('3');
+			gamma.setMinValue('0.01');
+			gamma.setValue('1');
+			gamma.setStep('0.01');
+			gamma.setLabel('Gamma');
+			gamma.append();
+		}
+
+		this.setGamma = function(canvasInstance) {
+			var context = canvasInstance.getContext();
+			var contrastSlider = document.getElementById('gammaSlider');
+			var imageData = context.getImageData(0, 0, canvasInstance.getWidth(), canvasInstance.getHeight());
+			var data = imageData.data;
+			var copyData = imageData.data.slice();
+			
+			gammaSlider.oninput = function(e) {
+				var sliderValue = parseFloat(e.target.value);
+				var temp = gammaManipulation(sliderValue, data);
+				restoreImageData(data, temp);
+				context.putImageData(imageData, 0, 0);
+				restoreImageData(data,copyData);
+			}
+		}
+
+		var restoreImageData = function(data, copyData) {
+			for(var i =0; i < data.length; i+=4){
+				data[i] = copyData[i];
+				data[i+1] = copyData[i+1];
+				data[i+2] = copyData[i+2];
+			}
+		}
+
+		var gammaManipulation = function(s, _data) {
+				var correctionFactor = getCorrectionFactor(s);
+				for(var i=0; i< _data.length; i+=4) {			
+					_data[i] = 255 * (Math.pow((_data[i] / 255), correctionFactor))//R
+					_data[i+1] = 255 * (Math.pow((_data[i+1] / 255), correctionFactor)) //G
+					_data[i+2] = 255 * (Math.pow((_data[i+2] / 255), correctionFactor))//B
+				}
+				return _data
+		}
+
+
+		var getCorrectionFactor = function(value) {
+			return (1 / value);
+		} 
+
+	}
+	return {
+
+		getInstance: function() {
+			if(instance == null) {
+				instance = new Gamma();
+			}
+			return instance;
+		}
+	}
+})();
+
+// var Saturation = (function() {
+// 	var instance;
+// 	function Saturation() {
+// 		var saturation = Manipulator.getInstance();
+		
+// 		this.init = function() {
+// 			saturation.init();
+// 			saturation.setId('saturationSlider');
+// 			saturation.setType('range');
+// 			saturation.setMaxValue('1.5');
+// 			saturation.setMinValue('1');
+// 			saturation.setValue('1');
+// 			saturation.setStep('0.01');
+// 			saturation.setLabel('Saturation');
+// 			saturation.append();
+// 		}
+
+// 		this.setSaturation = function(canvasInstance) {
+// 			var context = canvasInstance.getContext();
+// 			var contrastSlider = document.getElementById('saturationSlider');
+// 			var imageData = context.getImageData(0, 0, canvasInstance.getWidth(), canvasInstance.getHeight());
+// 			var data = imageData.data;
+// 			var copyData = imageData.data.slice();
+// 			var copiedImageData = {};
+// 			//saturation constants 
+// 			var totalRange = 2;
+// 			var previousValue=1;
+// 			saturationSlider.oninput = function(e) {
+// 				var slideLeft = false;
+// 				var currentValue;
+				
+// 				var s = parseFloat(e.target.value);
+// 				var currentValue = parseFloat(e.target.value);
+// 				if(s >= 1 && previousValue < currentValue) {
+// 					slideLeft = true;
+// 				}else {
+// 					slideLeft = false;
+// 				}
+
+// 				if(slideLeft) {
+// 					copiedImageData[s] = copyData;
+//  					saturationManipulation(s, data);
+//  					copyData = imageData.data.slice();
+// 				}
+
+// 				if(!slideLeft) {
+// 					restoreImageData(data, copiedImageData[s]);
+// 				}
+
+// 				previousValue = currentValue;
+// 				context.putImageData(imageData, 0, 0);
+// 			}
+// 		}
+
+// 		var restoreImageData = function(data, copyData) {
+// 			console.log('inside restore image');
+// 			for(var i =0; i < data.length; i+=4){
+// 				data[i] = copyData[i];
+// 				data[i+1] = copyData[i+1];
+// 				data[i+2] = copyData[i+2];
+// 			}
+// 		}
+
+// 		var saturationManipulation = function(s,data) {
+// 			var RW = 0.299;
+// 			var RG = 0.587;
+// 			var RB = 0.114;
+// 			var sBar = parseFloat(1 - s);					
+// 			var a =  sBar * RW + s;
+// 			var b =  sBar * RW;
+// 			var c =  sBar * RW;
+// 			var d =  sBar * RG;
+// 			var e = sBar * RG + s;
+// 			var f = sBar * RG;
+// 			var g = sBar * RB;
+// 			var h = sBar * RB;
+// 			var itemp = sBar * RB + s;
+// 			for(var i=0; i< data.length; i+=4) {
+// 				var tempRed = data[i];
+// 				var tempGreen = data[i+1];
+// 				var tempBlue = data[i+2];
+// 				data[i] = a * tempRed + d * tempGreen + g * tempBlue//R
+// 				data[i+1] = b * tempRed + e * tempGreen + h * tempBlue//G
+// 				data[i+2] = c * tempRed + f * tempGreen + itemp * tempBlue//B
+// 			}
+// 		}
+// 	}
+// 	return {
+
+// 		getInstance: function() {
+// 			if(instance == null) {
+// 				instance = new Saturation();
+// 			}
+// 			return instance;
+// 		}
+// 	}
+// })();
+
+var Saturation = (function() {
+	var instance;
+	function Saturation() {
+		var saturation = Manipulator.getInstance();
+		
+		this.init = function() {
+			saturation.init();
+			saturation.setId('saturationSlider');
+			saturation.setType('range');
+			saturation.setMaxValue('2');
+			saturation.setMinValue('0');
+			saturation.setValue('1');
+			saturation.setStep('0.01');
+			saturation.setLabel('Saturation');
+			saturation.append();
+		}
+
+		this.setSaturation = function(canvasInstance) {
+			var context = canvasInstance.getContext();
+			var contrastSlider = document.getElementById('saturationSlider');
+			var imageData = context.getImageData(0, 0, canvasInstance.getWidth(), canvasInstance.getHeight());
+			var data = imageData.data;
+			var copyData = imageData.data.slice();
+			
+			//saturation constants 
+			var previousValue=1;
+			saturationSlider.oninput = function(e) {
+				var sliderValue = parseFloat(e.target.value);
+				var temp = saturationManipulation(sliderValue, data);
+				restoreImageData(data, temp);
+				context.putImageData(imageData, 0, 0);
+				restoreImageData(data,copyData);
+			}
+		}
+
+		var restoreImageData = function(data, copyData) {
+			for(var i =0; i < data.length; i+=4){
+				data[i] = copyData[i];
+				data[i+1] = copyData[i+1];
+				data[i+2] = copyData[i+2];
+			}
+		}
+
+		var saturationManipulation = function(s, _data) {
+			console.log('saturation manipulator');
+			var RW = 0.299;
+			var RG = 0.587;
+			var RB = 0.114;
+			var sBar = parseFloat(1 - s);					
+			var a =  sBar * RW + s;
+			var b =  sBar * RW;
+			var c =  sBar * RW;
+			var d =  sBar * RG;
+			var e = sBar * RG + s;
+			var f = sBar * RG;
+			var g = sBar * RB;
+			var h = sBar * RB;
+			var itemp = sBar * RB + s;
+			for(var i=0; i< _data.length; i+=4) {
+				var tempRed = _data[i];
+				var tempGreen = _data[i+1];
+				var tempBlue = _data[i+2];
+				_data[i] = a * tempRed + d * tempGreen + g * tempBlue//R
+				_data[i+1] = b * tempRed + e * tempGreen + h * tempBlue//G
+				_data[i+2] = c * tempRed + f * tempGreen + itemp * tempBlue//B
+			}
+			return _data;
+		}
+	}
+	return {
+
+		getInstance: function() {
+			if(instance == null) {
+				instance = new Saturation();
+			}
+			return instance;
+		}
+	}
+})();
+
+
+var Temperature = (function() {
+	var instance;
+	function Temperature() {
+		var temperature = Manipulator.getInstance();
+		
+		this.init = function() {
+			temperature.init();
+			temperature.setId('temperatureSlider');
+			temperature.setType('range');
+			temperature.setMaxValue('40');
+			temperature.setMinValue('-40');
+			temperature.setValue('0');
+			temperature.setLabel('Temperature');
+			temperature.append();
+		}
+
+		this.setTemperature = function(canvasInstance) {
+			var context = canvasInstance.getContext();
+			var temperatureSlider = document.getElementById('temperatureSlider');
+			var imageData = context.getImageData(0, 0, canvasInstance.getWidth(), canvasInstance.getHeight());
+			var data = imageData.data;
+			var copyData = imageData.data.slice();
+			
+			temperatureSlider.oninput = function(e) {
+				var sliderValue = parseFloat(e.target.value);
+				var temp = temperatureManipulation(sliderValue, data);
+				restoreImageData(data, temp);
+				context.putImageData(imageData, 0, 0);
+				restoreImageData(data,copyData);
+			}
+		}
+
+		var restoreImageData = function(data, copyData) {
+			for(var i =0; i < data.length; i+=4){
+				data[i] = copyData[i];
+				data[i+1] = copyData[i+1];
+				data[i+2] = copyData[i+2];
+			}
+		}
+
+		var temperatureManipulation = function(s, _data) {
+			for(var i=0; i< _data.length; i+=4) {
+				_data[i] = Util.truncate(_data[i] + s)//R
+				//_data[i+1] = //G
+				_data[i+2] =Util.truncate(_data[i+2] - s)//B
+			}
+			return _data;
+		}
+	}
+	return {
+
+		getInstance: function() {
+			if(instance == null) {
+				instance = new Temperature();
+			}
+			return instance;
+		}
+	}
+})();
+
+var Tint = (function() {
+	var instance;
+	function Tint() {
+		var tint = Manipulator.getInstance();
+		
+		this.init = function() {
+			tint.init();
+			tint.setId('tintSlider');
+			tint.setType('range');
+			tint.setMaxValue('40');
+			tint.setMinValue('-40');
+			tint.setValue('0');
+			tint.setLabel('Tint');
+			tint.append();
+		}
+
+		this.setTint = function(canvasInstance) {
+			var context = canvasInstance.getContext();
+			var tintSlider = document.getElementById('tintSlider');
+			var imageData = context.getImageData(0, 0, canvasInstance.getWidth(), canvasInstance.getHeight());
+			var data = imageData.data;
+			var copyData = imageData.data.slice();
+			
+			tintSlider.oninput = function(e) {
+				var sliderValue = parseFloat(e.target.value);
+				var temp = tintManipulation(sliderValue, data);
+				restoreImageData(data, temp);
+				context.putImageData(imageData, 0, 0);
+				restoreImageData(data,copyData);
+			}
+		}
+
+		var restoreImageData = function(data, copyData) {
+			for(var i =0; i < data.length; i+=4){
+				data[i] = copyData[i];
+				data[i+1] = copyData[i+1];
+				data[i+2] = copyData[i+2];
+			}
+		}
+
+		var tintManipulation = function(s, _data) {
+			for(var i=0; i< _data.length; i+=4) {
+				_data[i] = _data[i]//R
+				_data[i+1] = Util.truncate(_data[i] + s) //G
+				_data[i+2] =_data[i+2]//B
+			}
+			return _data;
+		}
+	}
+	return {
+
+		getInstance: function() {
+			if(instance == null) {
+				instance = new Tint();
 			}
 			return instance;
 		}
--- js/script.js
+++ js/script.js
@@ -5,23 +5,38 @@
 
 	var file = document.getElementById('getFile');
 	file.addEventListener("change",fileSelectHandler, false);
-	console.log(document.querySelector('input[type=file]').files[0]);
+	
 	var brightness = Brightness.getInstance();
 	brightness.init();
+
 	var contrast = Contrast.getInstance();
 	contrast.init();
+
+	var saturation = Saturation.getInstance();
+	saturation.init();
+
+	var gamma = Gamma.getInstance();
+	gamma.init();
+
+	var temperature = Temperature.getInstance();
+	temperature.init();
+
+	var tint = Tint.getInstance();
+	tint.init();
+
 
 	function fileSelectHandler(event) {
 		if(file) {
 			handleFile(event.target.files[0]);				
 			setTimeout(function(){
-				brightnessTest();
-				shadowTest();
 
 				brightness.setBrightness(InstaUi.getInstance());
 				contrast.setContrast(InstaUi.getInstance());
-				
-			},3000);
+				gamma.setGamma(InstaUi.getInstance());
+				saturation.setSaturation(InstaUi.getInstance());
+				temperature.setTemperature(InstaUi.getInstance());
+				tint.setTint(InstaUi.getInstance());
+			},2000);
 
 		}
 	}
@@ -40,13 +55,12 @@
 			var height = img.height;
 			var aspectRatio = width/height;
 			var manipulationWidth = Util.getStyle(Util.getElement('col-2-left'),'width');
-			console.log('manipulationWidth',manipulationWidth);
 			imageData.setImageHeight(IMAGE_HEIGHT);
 			imageData.setImageWidth(aspectRatio * IMAGE_HEIGHT);
 			var dx = (manipulationWidth - (aspectRatio * IMAGE_HEIGHT))/2;
 			img.addEventListener('load', function(event){
 				context.drawImage(
-					img, 0, 0, width, height, dx, 0, aspectRatio * IMAGE_HEIGHT, IMAGE_HEIGHT);
+					img, 0, 0, width, height, 0, 0, aspectRatio * IMAGE_HEIGHT, IMAGE_HEIGHT);
 			});
 		});
 		if(file){
@@ -63,112 +77,7 @@
 		}
 	}
 
-	var brightnessTest = function() {
-		var brightness = document.getElementById('brightness');
-		var image = ImageData.getInstance().getImage();
-		var canvasInstance = InstaUi.getInstance();
-		var context = canvasInstance.getContext();
-		var imageData = context.getImageData(0, 0, canvasInstance.getWidth(), canvasInstance.getHeight());
-		var originalData = imageData.data.slice();
-		var data = imageData.data;
-		//image.style.display = 'none';
-		var previousValue =0;
-		brightness.oninput = function(e) {
-			var currentValue = brightness.value;
-			var step = 0;
-			if(currentValue >= 0 && previousValue < currentValue) {
-				step = step + (currentValue - previousValue);
-				step *= 1;
-			}  
-			if(previousValue >= currentValue) {
-				step = step + (previousValue - currentValue);
-				step *= -1;
-			}
-			previousValue = currentValue;
-			console.log("step",step);
-			for(var i=0; i< data.length; i+=4) {
-				data[i] = Util.truncate(data[i] + step)//R
-				data[i+1] =Util.truncate(data[i+1] + step) //G
-				data[i+2] =Util.truncate(data[i+2] + step)//B
-			}
-			 context.putImageData(imageData, 0, 0);
-		}
 
-	}
-
-	var shadowTest = function() {
-		var shadow = document.getElementById('shadow');
-		//var image = ImageData.getInstance().getImage();
-		var canvasInstance = InstaUi.getInstance();
-		//var canvas = canvasInstance.getCanvas(); 
-		var context = canvasInstance.getContext();
-		var imageData = context.getImageData(0, 0, canvasInstance.getWidth(), canvasInstance.getHeight());
-		var originalData = imageData.data.slice();
-		var data = imageData.data;
-		//image.style.display = 'none';
-		var previousValue =0;
-		shadow.oninput = function(e) {
-			var currentValue = shadow.value;
-			console.log('shadow value',currentValue);
-			var step = 0;
-			var slideLeft = false;
-			console.log("previousValue",previousValue);
-			console.log("currentValue",currentValue);
-
-			if(currentValue >= 0 && previousValue >= currentValue) {
-				slideLeft = true;
-			}else if(currentValue < 0 && previousValue <= currentValue) {
-				slideLeft = true;
-			}else {
-				slideLeft = false;
-			}
-
-			if(slideLeft) {
-				step = step + (currentValue - previousValue);
-				step *=1;
-			}
-			if(!slideLeft) {
-				step = step + (previousValue - currentValue);
-				step *=-1;
-			}
-			previousValue = currentValue;
-			console.log('slideLeft',slideLeft);
-
-			// if(slideLeft) {
-			// 	step = step + (currentValue - previousValue);
-			// 	step *= 1;
-			// }
-
-			// if(!slideLeft) {
-			// 	step = step + (Math.abs(previousValue) - Math.abs(currentValue));
-			// 	step *= -1
-			// }
-
-
-			// if(currentValue >= 0 && previousValue < currentValue) {
-			// 	step = step + (currentValue - previousValue);
-			// 	step *= 1;
-			// 	slideLeft = true;
-			// }  
-			// if(previousValue >= currentValue) {
-			// 	step = step + (previousValue - currentValue);
-			// 	step *= -1;
-			// }
-			
-			for(var i=0; i< data.length; i+=4) {
-				// data[i] = Util.truncate(data[i] + brightnessValue)//R
-				// data[i+1] = Util.truncate(data[i+1] + brightnessValue) //G
-				// data[i+2] = Util.truncate(data[i+2] + brightnessValue)//B
-				data[i] = Util.truncate(data[i] + step)//R
-				data[i+1] =Util.truncate(data[i+1] + step) //G
-				data[i+2] =Util.truncate(data[i+2] + step)//B
-			}
-			 context.putImageData(imageData, 0, 0);
-		}
-	}
-
-	
-	
 createFilters();
 	
 	
